628d3b8df20941b6f1bc951af9910559fd4a9dde
Yg070212 <winter070212@gmail.com>
2024-09-24 21:44:12 +0900

    24-09-24 <GAME1>

--- Source.c
+++ Source.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-	printf("☆가위 바위 보 게임☆");
+	int Player = 0;
+	int Com = 0;
+	int round = 0;
+	int result[3] = { 0, };
+	// result [0] 무승부!, result [1] 플레이어가 이겼습니다!, result 플레이어가 패배하였습니다!
+	srand(time(NULL));
 
-
-
-	// if(PlayerChoice == AIChoice)
-
-	return 0;
 }
