23d4e56fd82920de919e914b77c45357a309ec66
Yg070212 <winter070212@gmail.com>
2024-09-30 22:07:02 +0900

    24-09-30 <Game1>

--- Source.c
+++ Source.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
+
+void textcolor(int colorNum)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),colorNum);
+}
+
+enum ColorType
+{
+	BLUE = 0, GREEN = 1, SkyBlue = 2, RED = 3, YELLOW = 4, PURPLE = 5, DarkRed = 6
+}COLOR;
 
 int main(void)
 {
@@ -11,10 +22,12 @@ int main(void)
 	// result [0] 무승부!, result [1] 플레이어가 승리하였습니다!, result 플레이어가 패배하였습니다!
 	srand(time(NULL)); // 랜덤값을 초기화
 
+	textcolor(5);
 	printf("가위 바위 보 게임을 시작해보세요!\n");
 	while (Player != -1)
 	{
 		Com = rand() % 3 + 1;
+		textcolor(6);
 		printf("이 중 하나를 선택하세요! 가위(1), 바위(2), 보(3) \n=>");
 		scanf_s("%d", &Player);
 
@@ -31,6 +44,7 @@ int main(void)
 
 			if (Com == Player)
 			{
+				textcolor(3);
 				printf("무승부!\n");
 				result[0]++;
 			}
@@ -38,6 +52,7 @@ int main(void)
 				(Player == 2 && Com == 1) ||	
 				(Player == 3 && Com == 2)))
 			{
+				textcolor(2);
 				printf("플레이어가 승리하였습니다!\n\n");
 				result[1]++;
 			}
@@ -45,14 +60,17 @@ int main(void)
 				(Com == 2 && Player == 1) ||
 				(Com == 3 && Player == 2)))
 			{
+				textcolor(4);
 				printf("플레이어가 패배하였습니다!\n\n");
 				result[2]++;
 			}
 		}
 		else
 		{
+			textcolor(7);
 			printf("잘못된 입력입니다. 1, 2, 3 중 하나를 입력해주세요!\n");
 			Com = rand() % 3 + 1;
+			//textcolor(7);
 			printf("이 중 하나를 선택하세요! 가위(1), 바위(2), 보(3) \n=>");
 			scanf_s("%d", &Player);
 
