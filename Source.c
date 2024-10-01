0110932b72a62029e5c3718cff87528584187495
Yg070212 <winter070212@gmail.com>
2024-10-01 21:43:38 +0900

    24-10-01 <R.S.P Game Beta>

--- Source.c
+++ Source.c
@@ -3,6 +3,16 @@
 #include <time.h>
 #include <windows.h>
 
+void Position(int x, int y)
+{
+	COORD coord;
+
+	coord.X = x;
+	coord.Y = y;
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
 void textcolor(int colorNum)
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),colorNum);
@@ -10,7 +20,22 @@ void textcolor(int colorNum)
 
 enum ColorType
 {
-	BLUE = 0, GREEN = 1, SkyBlue = 2, RED = 3, YELLOW = 4, PURPLE = 5, DarkRed = 6
+	BLACK,  	//0
+	darkBLUE,	//1
+	DarkGreen,	//2
+	darkSkyBlue,//3
+	DarkRed,  	//4
+	DarkPurple,	//5
+	DarkYellow,	//6
+	GRAY,		//7
+	DarkGray,	//8
+	BLUE,		//9
+	GREEN,		//10
+	SkyBlue,	//11
+	RED,		//12
+	PURPLE,		//13
+	YELLOW,		//14
+	WHITE		//15
 }COLOR;
 
 int main(void)
@@ -22,13 +47,29 @@ int main(void)
 	// result [0] 무승부!, result [1] 플레이어가 승리하였습니다!, result 플레이어가 패배하였습니다!
 	srand(time(NULL)); // 랜덤값을 초기화
 
-	textcolor(5);
-	printf("가위 바위 보 게임을 시작해보세요!\n");
 	while (Player != -1)
 	{
+		textcolor(14);
+		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		printf("                               ________________________________________________________________________________________________________________________________________ \n");
+		printf("                              |                                                                                                                                        |\n");
+		printf("                              |     # # # # #   #            #  #        #            #         #   #             #  #       #              #             #       #    |\n");
+		printf("                              |            #   #           #     #      #            #         #   #            #     #     #              # # # # # # # #       #     |\n");
+		printf("                              |           #   # # #         #  #       #            #         #   # # # #        #  #      #              #             #       #      |\n");
+		printf("                              |         #    #          # # # # # #   #            # # # # # #   #           # # # # # #  #              # # # # # # # #       #       |\n");
+		printf("                              |       #     #              #         #            #         #   #                 #      #                     #                       |\n");
+		printf("                              |     #      #              #         #            # # # # # #   #                 #      #              # # # # # # # #       #         |\n");
+		printf("                              |                                                                                                                                        |\n");
+		printf("                              |________________________________________________________________________________________________________________________________________|\n");
+
+		textcolor(10);
+		printf("\n\n\n\n\n\n\n");
+		printf("                                                                             가위 바위 보 게임을 시작해보세요!\n\n");
+
 		Com = rand() % 3 + 1;
-		textcolor(6);
-		printf("이 중 하나를 선택하세요! 가위(1), 바위(2), 보(3) \n=>");
+		textcolor(14);
+		printf("                                                                      이 중 하나를 선택하세요! 가위(1), 바위(2), 보(3) \n");
+		printf("                                                                      =>");
 		scanf_s("%d", &Player);
 
 		getchar();
@@ -44,34 +85,56 @@ int main(void)
 
 			if (Com == Player)
 			{
-				textcolor(3);
-				printf("무승부!\n");
+				textcolor(13);
+				printf("무승부!\n"); 
+				Position(80, -36);
 				result[0]++;
 			}
 			else if (((Player == 1 && Com == 3) ||
 				(Player == 2 && Com == 1) ||	
 				(Player == 3 && Com == 2)))
 			{
-				textcolor(2);
+				textcolor(11);
 				printf("플레이어가 승리하였습니다!\n\n");
+				Position(80, -36);
 				result[1]++;
 			}
 			else if (((Com == 1 && Player == 3) ||
 				(Com == 2 && Player == 1) ||
 				(Com == 3 && Player == 2)))
 			{
-				textcolor(4);
+				textcolor(12);
 				printf("플레이어가 패배하였습니다!\n\n");
+				Position(80, -36);
 				result[2]++;
 			}
 		}
 		else
 		{
-			textcolor(7);
+			textcolor(4);
 			printf("잘못된 입력입니다. 1, 2, 3 중 하나를 입력해주세요!\n");
 			Com = rand() % 3 + 1;
-			//textcolor(7);
-			printf("이 중 하나를 선택하세요! 가위(1), 바위(2), 보(3) \n=>");
+			textcolor(14);
+			printf("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+			printf("                               ________________________________________________________________________________________________________________________________________ \n");
+			printf("                              |                                                                                                                                        |\n");
+			printf("                              |     # # # # #   #            #  #        #            #         #   #             #  #       #              #             #       #    |\n");
+			printf("                              |            #   #           #     #      #            #         #   #            #     #     #              # # # # # # # #       #     |\n");
+			printf("                              |           #   # # #         #  #       #            #         #   # # # #        #  #      #              #             #       #      |\n");
+			printf("                              |         #    #          # # # # # #   #            # # # # # #   #           # # # # # #  #              # # # # # # # #       #       |\n");
+			printf("                              |       #     #              #         #            #         #   #                 #      #                     #                       |\n");
+			printf("                              |     #      #              #         #            # # # # # #   #                 #      #              # # # # # # # #       #         |\n");
+			printf("                              |                                                                                                                                        |\n");
+			printf("                              |________________________________________________________________________________________________________________________________________|\n");
+
+			textcolor(10);
+			printf("\n\n\n\n\n\n\n");
+			printf("                                                                             가위 바위 보 게임을 시작해보세요!\n\n");
+
+			Com = rand() % 3 + 1;
+			textcolor(14);
+			printf("                                                                      이 중 하나를 선택하세요! 가위(1), 바위(2), 보(3) \n");
+			printf("                                                                      =>");
 			scanf_s("%d", &Player);
 
 			getchar();
@@ -80,8 +143,11 @@ int main(void)
 	}
 
 	printf("게임 종료! 결과 → ");
+	textcolor(13);
 	printf("무승부 : %d회\n", result[0]);
+	textcolor(11);
 	printf("                   승리 : %d회\n", result[1]);
+	textcolor(12);
 	printf("                   패배 : %d회\n", result[2]);
 
 	return 0;
